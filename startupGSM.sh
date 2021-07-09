214944da2adb79243c2ad518f345efd19b066c1f
YariLei <57292320+YariLei@users.noreply.github.com>
2021-07-09 17:59:21 +0300

    startup

---
+++ startupGSM.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+gpio mode 7 output
+gpio write 7 0
+sleep 1
+gpio write 7 1
+gpio mode 7 input
+sleep 5
+
+cd /home/pi/GSMtracker
+
+while :
+do
+	python GSMmodule.py
+	sleep 5
+done
+
